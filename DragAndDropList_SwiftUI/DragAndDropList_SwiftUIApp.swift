4389018fec2e7310e506c55c5935a53e57c9c4e4
SeongMin-K <eo1tks3@naver.com>
2022-01-03 17:45:12 +0900

    Initial Commit

---
+++ DragAndDropList_SwiftUI/DragAndDropList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropList_SwiftUIApp.swift
+//  DragAndDropList_SwiftUI
+//
+//  Created by SeongMinK on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
